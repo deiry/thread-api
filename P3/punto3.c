83b9f984abfb00e1227423e99b19c5e3877eb7d3
deiry <sofia.navas96@gmail.com>
2020-04-18 17:42:40 -0500

    punto3

--- P3/punto3.c
+++ P3/punto3.c
@@ -1,38 +1,48 @@
-/*El siguiente programa cuenta el número de veces que el carácter a o A 
-aparece en el fichero indicado como parámetro de entrada. Modifíquese 
-para que ahora se cree un hilo y sea este el que ejecute la función cuenta.*/
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <pthread.h>
 
 #define MAXLON 1000
 
-void cuenta (char *);
+void *cuenta(void *);
 
-int main (int argc, char *argv[]) { 
-  if (argc!= 2) {
-    printf ("Indica el nombre de un fichero.\n");
-    exit(0);
-  }
-  cuenta(argv[1]);
-  return 0;
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Indica el nombre de un fichero.\n");
+        exit(0);
+    }
+
+    pthread_t tid;
+    
+    pthread_create(&tid, NULL, cuenta, &argv[1]);
+    pthread_join(tid, NULL);
+    return 0;
 }
 
-void cuenta (char *nombre) {
-  int pos, cont= 0, leidos;
-  char cadena[MAXLON];
-  int fd;
-  fd= open(nombre,O_RDONLY);
-  while ((leidos =read(fd,cadena,MAXLON))!= 0) {
-    for (pos= 0; pos< leidos; pos++) {
-      if ((cadena[pos]== 'a') || (cadena[pos]== 'A')) {
-        cont++;
-      }
+void *cuenta(void *nombre)
+{
+    int pos, cont = 0, leidos;
+    char cadena[MAXLON];
+    int fd;
+    fd = open(nombre, O_RDONLY);
+    while ((leidos = read(fd, cadena, MAXLON)) != 0)
+    {
+        for (pos = 0; pos < leidos; pos++)
+        {
+            if ((cadena[pos] == 'a') || (cadena[pos] == 'A'))
+            {
+                printf("%d",cont);
+                cont++;
+            }
+        }
     }
-  }
-  printf("Fichero %s: %d caracteres 'a' o 'A' encontrados\n", nombre, cont);
-  close(fd);
+    printf("Fichero %s: %d caracteres 'a' o 'A' encontrados\n", nombre, cont);
+    close(fd);
+    return 0;
 }
