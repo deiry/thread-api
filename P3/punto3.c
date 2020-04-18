a7b18ab3b5d289770ade5cf992b5d12f03520b50
deiry <sofia.navas96@gmail.com>
2020-04-18 18:10:36 -0500

    intento punto 3 :c

--- P3/punto3.c
+++ P3/punto3.c
@@ -8,6 +8,7 @@
 
 #define MAXLON 1000
 
+pthread_t tid;
 void *cuenta(void *);
 
 int main(int argc, char *argv[])
@@ -17,32 +18,26 @@ int main(int argc, char *argv[])
         printf("Indica el nombre de un fichero.\n");
         exit(0);
     }
-
-    pthread_t tid;
-    
     pthread_create(&tid, NULL, cuenta, &argv[1]);
     pthread_join(tid, NULL);
-    return 0;
+
 }
 
-void *cuenta(void *nombre)
+void *cuenta(void* nombre)
 {
-    int pos, cont = 0, leidos;
+    int pos, cont= 0, leidos;
     char cadena[MAXLON];
     int fd;
-    fd = open(nombre, O_RDONLY);
-    while ((leidos = read(fd, cadena, MAXLON)) != 0)
-    {
-        for (pos = 0; pos < leidos; pos++)
-        {
-            if ((cadena[pos] == 'a') || (cadena[pos] == 'A'))
-            {
-                printf("%d",cont);
-                cont++;
-            }
+    fd= open(nombre,O_RDONLY);
+    while ((leidos =read(fd,cadena,MAXLON))!= 0) {
+        printf(" %d\n", leidos);
+        for (pos= 0; pos< leidos; pos++) {
+        if ((cadena[pos]== 'a') || (cadena[pos]== 'A')) {
+            cont++;
+        }
         }
     }
     printf("Fichero %s: %d caracteres 'a' o 'A' encontrados\n", nombre, cont);
     close(fd);
-    return 0;
+    return NULL;
 }
