f1a0820bc24fb71e1c0970275ef345ee45797271
deiry <sofia.navas96@gmail.com>
2020-04-18 22:00:05 -0500

    added 5 and 6 fail

--- P4/punto4.c
+++ P4/punto4.c
@@ -2,3 +2,59 @@
  para que se creen tantos hilos como ficheros especificados 
  como parámetros de entrada de tal manera que todos los 
  hilos lleven a cabo su función de forma concurrente.*/
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+#define MAXLON 1000
+
+pthread_t tid;
+void *cuenta(void *);
+
+
+int main(int argc, char *argv[])
+{
+    pthread_t hilo[argc-1];
+    int param;
+    if (argc < 2)
+    {
+        printf("Indica el nombre de un fichero.\n");
+        exit(0);
+    }
+    for (param= 0; param< argc-1; param++){
+
+        pthread_create (&hilo[param], NULL, cuenta,(void *) argv[param+1]);
+    }
+
+    for (param= 0; param< argc-1; param++) {
+        pthread_join (hilo[param], NULL);
+        printf ("El hilo %d ha finalizado\n", param+1);
+    }
+    printf ("Ficheros leidos exitosamente\n");
+
+}
+
+void *cuenta(void *nombre)
+{
+    char *name= (char *)nombre;
+    int pos, cont= 0, leidos;
+    char cadena[MAXLON];
+    int fd;
+    fd= open(name,O_RDONLY);
+   
+    while ((leidos =read(fd,cadena,MAXLON))!= 0) {
+        
+        for (pos= 0; pos< leidos; pos++) {
+        if ((cadena[pos]== 'a') || (cadena[pos]== 'A')) {
+            cont++;
+        }
+        }
+    }
+    printf("Fichero %s: %d caracteres 'a' o 'A' encontrados\n", nombre, cont);
+    close(fd);
+    pthread_exit(NULL);
+}
--- P6/punto6.c
+++ P6/punto6.c
@@ -1,3 +1,70 @@
 /*El profesor de un curso desea un programa en lenguaje C que calcule 
 la desviación estándar (símbolo σ o s) de las notas obtenidas por sus 
 estudiantes en el curso */
+
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include<math.h>
+
+#define MAXLON 4
+
+void calculate_standard_deviation(float [], int);
+
+int main(int argc, char *argv[]){
+    if (argc!= 2) {
+        printf ("Indica el nombre de un fichero.\n");
+        exit(0);
+    }
+    float notas[3] = {4.5,4.5,3.0};
+    calculate_standard_deviation(&notas, sizeof(notas)/sizeof(notas[0]));
+    return 0;
+
+
+}
+
+void read_file(char *nombre){
+    char *name = (char *)nombre;
+    int pos, cont= 0, leidos;
+    char cadena[MAXLON];
+    int fd;
+    fd= open(name,O_RDONLY);
+   
+    while ((leidos =read(fd,cadena,MAXLON))!= 0) {
+        
+        for (pos= 0; pos< leidos; pos++) {
+        if ((cadena[pos]== 'a') || (cadena[pos]== 'A')) {
+            cont++;
+        }
+        }
+    }
+    printf("Fichero %s: %d caracteres 'a' o 'A' encontrados\n", nombre, cont);
+    close(fd);
+
+}
+
+void calculate_standard_deviation(float notas[], int n){
+  
+    int pos;
+    float sum=0.0,mean=0.0;
+
+    printf("size %d", n);
+    for (pos= 0; pos< n; pos++) {
+         sum = sum+notas[pos];
+    }
+
+    mean = sum/n;
+    printf("mean %f", mean);
+   
+    float sumatoria = 0.0;
+    for(pos=0; pos<n; pos++){
+        double aux = pow((notas[pos]-mean),2);
+        sumatoria = sumatoria + aux;
+    }
+    printf("sumatoria %f", sumatoria);
+    float std = sqrt(sumatoria/(n));
+    printf("std %f", std);
+
+
+}
