c687d118847c3a1d1d887bf59f043556acd4d9f1
Estefany <estefa942@gmail.com>
2020-04-19 19:57:42 -0500

    solved punto 5

--- P5/punto5Hilos.c
+++ P5/punto5Hilos.c
@@ -10,7 +10,6 @@
 #define MAXLON 1000
 int mitadVector = 0, cont = 0;
 char cadena[MAXLON];
-int  vector[MAXLON];
 pthread_t hilo1, hilo2;
 int fd, sumH1, sumH2, sum;
 int handleVector (char *);
@@ -60,18 +59,16 @@ int main (int argc, char *argv[]) {
     exit(0);
   }
   handleVector(argv[1]);
-  printf("entraaa y tiene mitad %d \n", mitadVector);
   pthread_create (&hilo1, NULL, sumHalfVectorH1, mitadVector);
   pthread_create (&hilo2, NULL, sumHalfVectorH2, mitadVector);
   pthread_join (hilo1, NULL);
   pthread_join (hilo2, NULL);
   sum = sumH1 + sumH2;
-  printf("suma: %d \n", sum);
+  printf("La suma de los elementos del archivo es: %d \n", sum);
   return 0;
 }
 
 int handleVector (char *nombre) {
-  printf("entra");
   int pos, leidos;
   fd= open(nombre,O_RDONLY);
   while ((leidos =read(fd,cadena,MAXLON))!= 0) {
@@ -79,31 +76,24 @@ int handleVector (char *nombre) {
       if ((cadena[pos] != ',') && (cadena[pos] != ' ') && (cadena[pos] != '\n')) {
         cont++;
         insertFirst(pos,cadena[pos]-'0');
-        vector[pos]= cadena[pos] - '0';
-        printf("vecot %d \n",vector[pos]);
       }
     }
   
   }
 
   mitadVector = floor(cont/2);
-  
- 
-  // printf("La suma de los números del fichero %s es:   \n", nombre);
   close(fd);
   return 0;
 }
 
 void * sumHalfVectorH1( void * mitad){
   int m =  (int) mitad;
-    printf("entraaa %lld \n", m);
   int pos;
   struct node *copy_head = head;
 
   for(pos = 0; pos < m; pos++){
     sumH1 = sumH1+copy_head->data;
     copy_head = copy_head->next;
-    printf("iter h1\n");
   }
 
   head = copy_head;
@@ -113,15 +103,12 @@ void * sumHalfVectorH1( void * mitad){
 
 void * sumHalfVectorH2( void * mitad){
    int m =  (int) mitad;
-    printf("H2 Cont %lld %d \n", cont, m);
-  struct node *copy_head = head;
-int pos;
-  for(pos = m; pos < cont; pos++){
+   struct node *copy_head = head;
+   int pos;
+   for(pos = m; pos < cont; pos++){
     sumH2 = sumH2+copy_head->data;
     copy_head = copy_head->next;
-    printf("iter h2\n");
   }
-
-   printf("Suma h2 %d finaliza h2\n", sumH2);
+  printf("Suma h2 %d finaliza h2\n", sumH2);
   pthread_exit(NULL);
 }
